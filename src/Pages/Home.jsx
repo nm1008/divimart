8ecc95665499ac42fe5243c3894f24e3b66df82a
nm1008 <mallari08nikko@yahoo.com>
2024-01-13 21:19:01 +0800

    improved ui for home page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -8,50 +8,44 @@ const Home = () => {
       </h1>
       <div className="h-[100vh] grid md:grid-cols-2 xl:grid-cols-3 gap-2 ">
         <Link
-          to='/products/furniture'
-          className="relative overflow-hidden col-span-2 hover:bg-gradient-to-tr from-slate-900 to-slate-500 "
+          to="/products/furniture"
+          className="relative overflow-hidden col-span-2  "
         >
           <img
             className="w-full h-full object-cover  "
             src="../src/images/furniture.jpg"
           />
-          <h1 className="absolute top-0 left-0 bg-slate-900 text-white p-3  font-semibold">
+          <h1 className="absolute top-0 left-0 bg-slate-900 text-white p-3 font-semibold">
             Furnitures
           </h1>
         </Link>
         <Link
           to="/products/skincare"
-          className="relative overflow-hidden row-span-2 hover:bg-gradient-to-tr from-slate-900 to-slate-500"
+          className="relative overflow-hidden row-span-2"
         >
           <img
             className="w-full h-full object-cover  "
             src="../src/images/skincare.jpg"
           />
-          <h1 className="absolute top-0 left-0 bg-slate-900 text-white p-3  font-semibold">
+          <h1 className="absolute top-0 left-0 bg-slate-900 text-white p-3 font-semibold">
             Skincare
           </h1>
         </Link>
-        <Link
-          to="/products/kitchen"
-          className="relative overflow-hidden hover:bg-gradient-to-tr from-slate-900 to-black-500 "
-        >
+        <Link to="/products/kitchen" className="relative overflow-hidden ">
           <img
             className="w-full h-full object-cover "
             src="../src/images/kitchen.jpg"
           />
-          <h1 className="absolute top-0 left-0 bg-slate-900 text-white p-3  font-semibold">
+          <h1 className="absolute top-0 left-0 bg-slate-900 text-white p-3 font-semibold">
             Kitchen
           </h1>
         </Link>
-        <Link
-          to='/products/gadgets'
-          className="relative overflow-hidden hover:bg-gradient-to-tr from-slate-900 to-black-900"
-        >
+        <Link to="/products/gadgets" className="relative overflow-hidden ">
           <img
             className="w-full h-full object-cover "
             src="../src/images/gadgets.jpg"
           />
-          <h1 className="absolute top-0 left-0 bg-slate-900 text-white p-3  font-semibold">
+          <h1 className="absolute top-0 left-0 bg-slate-900 text-white p-3 font-semibold">
             Gadgets
           </h1>
         </Link>
